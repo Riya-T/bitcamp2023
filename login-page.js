0bc93ec67853e76abaef1593d09aa7bd69139e11
Dakshita <dakshitapal902@gmail.com>
2023-04-08 10:55:08 -0400

    login

---
+++ login-page.js
@@ -0,0 +1,15 @@
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "web_dev") {
+        alert("You are now logged in!");
+        location.reload();
+    } 
+})
