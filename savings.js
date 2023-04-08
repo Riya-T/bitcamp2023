f716520e9c86295e677a99add16354759886b6d4
riyathapar <riyathapar06@gmail.com>
2023-04-08 19:24:24 -0400

    slight js update

--- savings.js
+++ savings.js
@@ -1,11 +1,15 @@
-
-
+let ready = false;
 function updateSavings() {
+    if (ready == true){
+        window.location.pathname = "Users/riyathapar/bitcamp2023/victory.html";
+    }
     let add = document.getElementById("number").value;
     if (!(isNaN(parseInt(add)))){
         document.getElementById("current").innerHTML = parseInt(document.getElementById("current").innerHTML) + parseInt(add);
     }
     if (parseInt(document.getElementById("current").innerHTML) >= parseInt(document.getElementById("goal").innerHTML)){
-        window.location.pathname = "Users/riyathapar/bitcamp2023/victory.html";
+        button = document.getElementById("savings-submit");
+        button.value = "Shoot for the Moon!";
+        ready = true;
     }
 }
